de3e5b23f6900176abc9cdbcbcd5786e24abc22f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-09-20 11:30:53 -0400

    fix(upgrade): upgraded deps for testing

--- dist/ng-alertify.js
+++ dist/ng-alertify.js
@@ -1,5 +1,5 @@
 /**
- ng-alertify@0.9.0
+ ng-alertify@0.9.1
  AngularJS wrapper around alertify popup library
  Gleb Bahmutov <gleb@kensho.com>
  https://github.com/kensho/ng-alertify
@@ -726,7 +726,7 @@
     .constant('meta', {
       name: 'ng-alertify',
       description: 'AngularJS wrapper around alertify popup library',
-      version: '0.9.0',
+      version: '0.9.1',
       author: 'Gleb Bahmutov <gleb@kensho.com>'
     })
     .factory('Alertify', ['$q', alertifyFactory]);
